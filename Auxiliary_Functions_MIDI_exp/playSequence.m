1814973e3584c2680b5bd0b89a9424a636c2f65d
jonathan sahar <jonathan.sahar@gmail.com>
2022-09-28 09:04:20 +0300

    fix play/sequence audio

--- Auxiliary_Functions_MIDI_exp/playSequence.m
+++ Auxiliary_Functions_MIDI_exp/playSequence.m
@@ -3,13 +3,14 @@ function playSequence(ear)
 filename = "./audio_files/sequence.mp3"
 [osc, dev_writer] = initializeAudioDevices();
 [y, fs] = audioread(filename);
-mute_waveform = zeros(length(y), 1);
+signal = y(:,1);
+mute_waveform = zeros(length(signal), 1);
 if strcmp(ear, 'R')
-    dev_writer([mute_waveform(), y]);
+    dev_writer([mute_waveform(), signal]);
 elseif strcmp(ear, 'L')
-    dev_writer([y, mute_waveform()]);
+    dev_writer([signal, mute_waveform()]);
 elseif strcmp(ear, 'both')
-    dev_writer(y);
+    dev_writer(signal);
 end
     release(osc);
     release(dev_writer);
