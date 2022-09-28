7f227bb2c1f7422d962da00d77ac74deda879861
jonathan sahar <jonathan.sahar@gmail.com>
2022-09-28 13:19:55 +0300

    working on he lab's lenovo laptop

--- Auxiliary_Functions_MIDI_exp/playMIDI.m
+++ Auxiliary_Functions_MIDI_exp/playMIDI.m
@@ -35,14 +35,28 @@ notes_vec = zeros(num_notes * 2, 1);
 timestamp_vec = zeros(num_notes * 2, 1);
 
 try
-%     receive midi input for num_notes
+    %     receive midi input for num_notes
     note_ctr = 1;
     while note_ctr <= num_notes
         if((toc(get_global_tic())) >= end_of_block_time)
-            timedOut = true;
             fprintf('************\nTime exceeded!\n************\n')
+
+            release(osc);
+            release(dev_writer);
+
             throw(MException('Yonatan:time exceeded','time exceeded'));
+        else
+            [keyIsDown, keyTime, keyCode] = KbCheck;
+            if keyCode(KbName('ESCAPE'))
+                fprintf('************\nEscape called!\n************\n')
+
+                release(osc);
+                release(dev_writer);
+                throw(MException('Yonatan:Escape called','Escape called'));
+
+            end
         end
+
         msgs = midireceive(midi_dev);
         for i = 1:numel(msgs)
             msg = msgs(i);
@@ -52,13 +66,13 @@ try
                 % synthesize an audio signal
                 osc.Frequency = note2Freq(msg.Note);
                 osc.Amplitude = msg.Velocity/127;
-                
+
                 % update data table with note pressed and timestamp
                 if i_block ~= 0 && msg.Note ~= 0 % not familiarization phase
                     notes_vec(note_ctr * 2 - 1) = msg.Note;
                     timestamp_vec(note_ctr * 2 - 1) = toc(get_global_tic); %msg.Timestamp;
                 end
-                
+
             elseif isNoteOff(msg)
                 if msg.Note == msg.Note
                     osc.Amplitude = 0;
@@ -75,26 +89,26 @@ try
                 end
             end
         end
-        
-    if bMute
-        dev_writer(mute_waveform);
-    else
-        if strcmp(ear, 'R')
-            dev_writer([mute_waveform, osc()]);
-        elseif strcmp(ear, 'L')
-            dev_writer([osc(), mute_waveform()]);
-        elseif strcmp(ear, 'both')
-            dev_writer(osc());
-        end
-    end
-    
-end
 
-time_of_last_note = toc(get_global_tic);
-duration_of_playing = time_of_last_note - time_of_first_note;
+        if bMute
+            dev_writer(mute_waveform);
+        else
+            if strcmp(ear, 'R')
+                dev_writer([mute_waveform, osc()]);
+            elseif strcmp(ear, 'L')
+                dev_writer([osc(), mute_waveform()]);
+            elseif strcmp(ear, 'both')
+                dev_writer(osc());
+            end
+        end
+
+    end % while
+
+    time_of_last_note = toc(get_global_tic);
+    duration_of_playing = time_of_last_note - time_of_first_note;
 
 catch
-end
+end % try/catch
 
 clear sound
 
